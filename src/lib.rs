3fd2ba83423beb8c007f0cddb5a2fe2ca64dd39a
PaddyDeng [鄧貴鍾] <paddydeng@ami.com>
2023-02-22 13:43:23 +0800

    brief completed

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+
+const CR: u8 = 0x0D;
+const LF: u8 = 0x0A;
+lazy_static! {
+    static ref CRLF_BUF: Vec<u8> = vec![CR, LF];
+    static ref LF_BUF: Vec<u8> = vec![LF];
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum LineEnding {
+    CRLF,
+    LF,
+}
+
+impl std::fmt::Display for LineEnding {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LineEnding::CRLF => write!(f, "crlf"),
+            LineEnding::LF => write!(f, "lf"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct CrlfStat {
+    lf: usize,
+    crlf: usize,
+}
+
+impl CrlfStat {
+    pub fn is_pure(&self) -> Option<LineEnding> {
+        if self.lf == 0 && self.crlf != 0 {
+            return Some(LineEnding::CRLF);
+        }
+        if self.lf != 0 && self.crlf == 0 {
+            return Some(LineEnding::LF);
+        }
+        return None;
+    }
+
+    pub fn lf(&self) -> usize {
+        self.lf
+    }
+
+    pub fn crlf(&self) -> usize {
+        self.crlf
+    }
+
+    pub fn new() -> CrlfStat {
+        CrlfStat { lf: 0, crlf: 0 }
+    }
+
+    pub fn measure_file<R: Read>(source: R) -> CrlfStat {
+        let mut reader = BufReader::new(source);
+        let mut buf = vec![];
+        let mut stat = CrlfStat::new();
+        loop {
+            match reader.read_until(LF, &mut buf) {
+                Ok(n) => {
+                    if n == 0 {
+                        break;
+                    }
+                }
+                Err(_) => break,
+            };
+            if buf.len() >= 2 && buf[buf.len() - 2] == CR {
+                stat.crlf += 1;
+            } else {
+                stat.lf += 1;
+            }
+            buf.clear();
+        }
+        return stat;
+    }
+}
+
+pub fn convert_to<R: Read, W: Write>(source: R, dest: W, ending: LineEnding) {
+    let mut source = BufReader::new(source);
+    let mut dest = BufWriter::new(dest);
+    let mut buf = vec![];
+
+    loop {
+        match source.read_until(LF, &mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+            }
+            Err(_) => break,
+        }
+        let has_line_ending = match buf.last() {
+            None => false,
+            Some(c) => *c == LF,
+        };
+        if has_line_ending {
+            if buf.len() >= 2 && buf[buf.len() - 2] == CR {
+                buf.pop();
+                buf.pop();
+            } else {
+                buf.pop();
+            }
+        }
+        dest.write(&buf).unwrap();
+        buf.clear();
+        if has_line_ending {
+            match ending {
+                LineEnding::CRLF => {
+                    dest.write(&CRLF_BUF).unwrap();
+                }
+                LineEnding::LF => {
+                    dest.write(&LF_BUF).unwrap();
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,137 @@
+use ansi_term::Color;
+use clap::{Arg, ArgAction, ArgMatches};
+use crlf::*;
+use std::{fs::File, path::PathBuf};
+
+fn build_args() -> ArgMatches {
+    clap::Command::new("utf8-test")
+    .author("paddydeng@ami.com")
+    .version(git_version::git_version!())
+    .about("Check if files has illegal character for given encoding")
+    .arg(
+      Arg::new("action")
+      .action(ArgAction::Set).required(true)
+    )
+    .arg(
+      Arg::new("pattern")
+      .default_value("**/*")
+      .action(ArgAction::Set)
+      .help("file name pattern (using glob)\nif --git-file(-g) is given, this pattern will be passed to git ls-files")
+    )
+    .arg(
+      Arg::new("git-file")
+      .short('g')
+      .long("git-file")
+      .action(ArgAction::SetTrue)
+      .help("Use git ls-files to get file list")
+    )
+    .arg(
+      Arg::new("verbose")
+      .short('v')
+      .long("verbose")
+      .action(ArgAction::SetTrue)
+      .help("Show detailed output")
+    )
+    .get_matches()
+}
+
+fn error_exit<T>(msg: String) -> T {
+    eprintln!("{}", msg);
+    std::process::exit(1);
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Action {
+    Measure,
+    SetCrlf,
+    SetLf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = build_args();
+    let action = match args.get_one::<String>("action").unwrap().as_str() {
+        "measure" => Action::Measure,
+        "to-crlf" => Action::SetCrlf,
+        "to-lf" => Action::SetLf,
+        _ => error_exit(format!("must provide an action: measure, to-crlf, to-lf")),
+    };
+    let pattern = args
+        .get_one::<String>("pattern")
+        .expect("No pattern provided");
+
+    let files: Vec<PathBuf> = if args.get_flag("git-file") {
+        let git_result = std::process::Command::new("git")
+            .args([
+                "ls-files",
+                "--",
+                if pattern == "**/*" { "*" } else { pattern },
+            ])
+            .output()
+            .expect("Run git command failed");
+        if !git_result.status.success() {
+            error_exit(format!(
+                "Git command failed: {}",
+                git_result
+                    .status
+                    .code()
+                    .expect("Get git status code failed")
+            ))
+        }
+        String::from_utf8_lossy(&git_result.stdout)
+            .split('\n')
+            .map(|s| s.trim())
+            .filter(|s| s.len() > 0)
+            .map(|l| PathBuf::from(l))
+            .collect()
+    } else {
+        glob::glob(pattern)
+            .expect("Failed to read glob pattern")
+            .map(|e| match e {
+                Ok(p) => p,
+                Err(e) => error_exit(format!("Glob match error {:?}", e)),
+            })
+            .filter(|f| f.is_file())
+            .collect()
+    };
+
+    if action == Action::Measure {
+        files.iter().for_each(|f| {
+            let stat = CrlfStat::measure_file(File::open(f).unwrap());
+            let filename = f.as_os_str().to_string_lossy();
+            let name_str = match stat.is_pure() {
+                Some(le) => match le {
+                    LineEnding::CRLF => Color::Yellow.paint(filename),
+                    LineEnding::LF => Color::Green.paint(filename),
+                },
+                None => Color::Red.paint(filename),
+            };
+            println!(
+                "{}: crlf: {}, lf: {}",
+                name_str,
+                Color::Yellow.paint(format!("{}", stat.crlf())),
+                Color::Green.paint(format!("{}", stat.lf()))
+            );
+        });
+    } else {
+        let target = match action {
+            Action::SetCrlf => LineEnding::CRLF,
+            Action::SetLf => LineEnding::LF,
+            _ => panic!("wtf"),
+        };
+
+        files.iter().for_each(|f| {
+            let mut dest = vec![];
+            convert_to(File::open(f).unwrap(), &mut dest, target);
+            std::fs::write(f, dest).unwrap();
+
+            println!(
+                "set {} to {}",
+                f.display(),
+                match target {
+                    LineEnding::CRLF => Color::Yellow.paint(format!("{}", target)),
+                    LineEnding::LF => Color::Green.paint(format!("{}", target)),
+                }
+            );
+        });
+    }
 }
